c02cd5aaaa96f70ab95a10b8a4a4ca7e86274698
cakeinpanic <cakeinpanic@gmail.com>
2019-06-04 21:57:56 +0300

    add inline keyboards

--- index.js
+++ index.js
@@ -10,17 +10,42 @@ const base = require('./database');
 const CHAT_ID = 123456;
 const utils = require('./utils');
 
+const yesno = {
+    parse_mode: "Markdown",
+    remove_keyboard: true,
+    reply_markup: {
+        inline_keyboard: [[{text: "Верно", callback_data: "true", switch_inline_query: "/sss"}], [{
+            text: "Я передумал",
+            callback_data: "false"
+        }]],
+        resize_keyboard: true,
+        one_time_keyboard: false,
+    },
+};
+
 // Matches "/echo [whatever]"
 bot.onText(/\/setgroup\s*(.+)/, (msg, match) => {
     if (!utils.isFromAdmin(msg)) {
         return;
     }
+    // bot.sendChatAction(msg.chat.id,)
     const groupNumber = match[1]; // the captured "whatever"
     base.addUserGroup(msg.from.id, groupNumber);
 });
 
+bot.on("callback_query", t => {
+    console.log(t);
+    bot.editMessageReplyMarkup(
+        {
+            parse_mode: "Markdown",
+        },
+        {
+            chat_id: t.message.chat.id,
+            message_id: t.message.message_id
+        });
+});
 // Matches "/echo [whatever]"
-bot.onText(/^send\s+(.+)/, (msg, match) => {
+bot.onText(/^[sS]end\s+(.+)/, (msg, match) => {
     const reply = msg.reply_to_message;
     const chatId = msg.chat.id;
 
@@ -28,10 +53,11 @@ bot.onText(/^send\s+(.+)/, (msg, match) => {
         return;
     }
 
-    bot.sendMessage(chatId,`sending to all participants from group ${match[1]} text:\n "${reply.text}"\nreply anything to this message to really send it!`).then(t=> {
+    bot.sendMessage(chatId, `sending to all participants from group ${match[1]} text:\n "${reply.text}"\n*reply anything to this message to really send it*!`, yesno).then(t => {
+        console.log(t);
         const id = bot.onReplyToMessage(chatId, t.message_id, () => {
             bot.removeReplyListener(id);
-            if(match[1] === 'all') {
+            if (match[1] === 'all') {
                 sendToAllUsers(reply.text);
                 return;
             }
@@ -42,24 +68,20 @@ bot.onText(/^send\s+(.+)/, (msg, match) => {
     });
 });
 
-// function subscribeOnNext
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-// bot.on('message', (msg) => {
-//     console.log(msg);
-//     if (utils.isFromAdmin(msg)) {
-//         processMessageFromAdmin(msg);
-//         return;
-//     }
-//
-//     if (utils.isFromUser(msg)) {
-//         processMessageFromUser(msg);
-//         return;
-//     }
-//
-//     processMessageInChat(msg);
-// });
+bot.on('message', (msg) => {
+    // console.log(msg);
+    // if (utils.isFromAdmin(msg)) {
+    //     processMessageFromAdmin(msg);
+    //     return;
+    // }
+    //
+    // if (utils.isFromUser(msg)) {
+    //     processMessageFromUser(msg);
+    //     return;
+    // }
+    //
+    // processMessageInChat(msg);
+});
 
 function processMessageFromAdmin(msg) {
 
@@ -83,12 +105,12 @@ function forwardMessageToAdminChat(fromChatId, messageId) {
     bot.forwardMessage(CHAT_ID, fromChatId, messageId)
 }
 
-function sendToAllUsersInTheGroup(msg, group){
-    base.groups[group].forEach(user=>{
+function sendToAllUsersInTheGroup(msg, group) {
+    base.groups[group].forEach(user => {
         bot.sendMessage(user, msg);
     })
 }
 
 function sendToAllUsers(msg) {
-    base.groups.forEach((g,i)=>sendToAllUsersInTheGroup(msg, i));
+    base.groups.forEach((g, i) => sendToAllUsersInTheGroup(msg, i));
 }
